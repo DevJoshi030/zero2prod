a3818e242d6d9dba75d894bb01b0bc03b2af305d
Dev Joshi <devjoshi030@gmail.com>
2023-05-18 23:30:16 +0530

    Commit before migrating to github

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-use actix_web::{dev::Server, get, post, web::Form, App, HttpResponse, HttpServer, Responder};
-use serde::Deserialize;
+use actix_web::{
+    dev::Server, get, post, web::Form, web::Json, App, HttpResponse, HttpServer, Responder,
+};
+use serde::{Deserialize, Serialize};
 use std::net::TcpListener;
 
 #[derive(Deserialize)]
@@ -8,14 +10,24 @@ struct FormData {
     name: String,
 }
 
+#[derive(Serialize)]
+struct Test<'a> {
+    email: &'a str,
+    name: &'a str,
+}
+
 #[get("/health_check")]
 async fn health_check() -> impl Responder {
-    HttpResponse::Ok().body("I am up!!!!!")
+    HttpResponse::Ok().json("I am up!!!!!")
 }
 
 #[post("/subscriptions")]
-async fn subscriptions(_form: Form<FormData>) -> impl Responder {
-    HttpResponse::Ok().finish()
+async fn subscriptions(_form: Json<FormData>) -> impl Responder {
+    let t = Test {
+        email: &_form.email,
+        name: &_form.name,
+    };
+    HttpResponse::Ok().json(t)
 }
 
 pub fn run(listener: TcpListener) -> std::io::Result<Server> {
